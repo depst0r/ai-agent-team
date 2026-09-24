a7632d374af18f385de0019b0320af21c11bdccb
sergey_panteleenko <depstor.work@gmail.com>
2026-09-24 17:35:08 +0300

    page-state

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
+'use client'
+import { useState } from "react";
 
 
 export default function Home() {
+
+  const [agent, setAgent] = useState( 'designer')
+  const [message, setMessage] = useState('')
+  const [reply, setReply] = useState('')
+  const [loading, setLoading] = useState(false)
+
+
   return (
     <>
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50  dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>Мой первый AI помошник </h1>
-        <p>Здесь будет чат</p>
+        <select value={agent} onChange={e => setAgent(e.target.value)  }>
+          <option value="designer">Дизайнер</option>
+          <option value="developer">Разработчик</option>
+          <option value="tester">Тестировщик</option>
+        </select>
+        <textarea 
+        rows={4} 
+        placeholder="Задание для агента" 
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+        ></textarea>
       </main>
     </div>
     </>
--- lib/agents/index.ts
+++ lib/agents/index.ts
@@ -1,6 +1,8 @@
 import { DESIGNER_PROMPT } from '@/lib/agents/designer'
 import { DEVELOPER_PROMPT } from '@/lib/agents/developer';
 import { TESTER_PROMPT } from '@/lib/agents/tester';
+import { DEFAULT_PROMPT } from './default';
+
 
 export const getPrompt = (agent: string) => {
 
@@ -8,7 +10,6 @@ export const getPrompt = (agent: string) => {
         case 'designer': return DESIGNER_PROMPT
         case 'developer': return DEVELOPER_PROMPT
         case 'tester': return TESTER_PROMPT
-        default:
-        return 'Выбрать агента'
+        default: return DEFAULT_PROMPT
     }
 }
