a732c0c74acf65d9bb24ce645f92d0cbc0d8aa9b
sergey_panteleenko <depstor.work@gmail.com>
2026-09-26 13:52:47 +0300

    26

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,13 +1,20 @@
 'use client'
 import { useState, ReactNode } from "react"
 
-export const CodeBlock = ({children, className} : {children: ReactNode, className?: string}) => {
+export const CodeBlock = ({children, className} : {children?: ReactNode, className?: string}) => {
 
     const [copied, setCopied] = useState(false)
 
     return  className ? (
         <div className="relative">
-            <button className="absolute top-2 right-2 cursor-pointer">копировать</button>
+            <button 
+            className="absolute top-2 right-2 px-2 py-1 cursor-pointer bg-zinc-800 text-zinc-100 border-2 border-zinc-600 shadow-[2px_2px_0_0_#000] active:translate-x-1 active:translate-y-1"
+            onClick={() => {
+                navigator.clipboard.writeText(String(children ?? ''))
+                setCopied(true)
+                setTimeout(() => setCopied(false), 2000)
+            }}
+            >{!copied ? 'копировать' : 'скопировано'}</button>
             <pre className="bg-zinc-950 overflow-x-auto text-sm p-4">
                 <code>{children}</code>
             </pre>
